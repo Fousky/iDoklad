e828549196ad325062fc7395813c92a867f22b87
Lukáš Brzák <lukas.brzak@aquadigital.cz>
2017-07-25 04:25:36 +0200

    Charge ProformaInvoice

---
+++ src/Functions/ProformaInvoices/ChargeProformaInvoice.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Fousky\Component\iDoklad\Functions\ProformaInvoices;
+
+use Fousky\Component\iDoklad\Functions\iDokladAbstractFunction;
+use Fousky\Component\iDoklad\Model\IssuedInvoices\IssuedInvoiceApiModel;
+
+/**
+ * @author Lukáš Brzák <lukas.brzak@aquadigital.cz>
+ */
+class ChargeProformaInvoice extends iDokladAbstractFunction
+{
+    protected $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Get iDokladModelInterface class.
+     *
+     * @see iDokladModelInterface
+     *
+     * @return string
+     */
+    public function getModelClass(): string
+    {
+        return IssuedInvoiceApiModel::class;
+    }
+
+    /**
+     * GET|POST|PUT|DELETE e.g.
+     *
+     * @see iDokladApiCaller::request()
+     *
+     * @return string
+     */
+    public function getHttpMethod(): string
+    {
+        return 'PUT';
+    }
+
+    /**
+     * Return base URI, e.g. /invoices; /invoice/1/edit and so on.
+     *
+     * @see iDokladApiClient::call()
+     *
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return sprintf('ProformaInvoices/%s/Account', $this->id);
+    }
+
+    /**
+     * Vrátí seznam parametrů, které se předají GuzzleHttp\Client
+     *
+     * @see \GuzzleHttp\Client::request()
+     * @see iDokladApiClient::call()
+     *
+     * @return array
+     */
+    public function getGuzzleOptions(): array
+    {
+        return [];
+    }
+}
---
+++ src/Model/IssuedInvoices/IssuedInvoiceApiModel.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Fousky\Component\iDoklad\Model\IssuedInvoices;
+
+use Fousky\Component\iDoklad\Model\iDokladAbstractModel;
+
+/**
+ * @method string|null getAttachmentFileName()
+ * @method string|null getBaseTaxBasicRate()
+ * @method string|null getBaseTaxBasicRateHc()
+ * @method string|null getBaseTaxReducedRate1()
+ * @method string|null getBaseTaxReducedRate1Hc()
+ * @method string|null getBaseTaxReducedRate2()
+ * @method string|null getBaseTaxReducedRate2Hc()
+ * @method string|null getBaseTaxZeroRate()
+ * @method string|null getBaseTaxZeroRateHc()
+ * @method string|null getConstantSymbolId()
+ * @method string|null getCurrencyId()
+ * @method string|null getDateLastChange()
+ * @method string|null getDateOfAccountingEvent()
+ * @method string|null getDateOfIssue()
+ * @method string|null getDateOfLastReminder()
+ * @method string|null getDateOfMaturity()
+ * @method string|null getDateOfPayment()
+ * @method string|null getDateOfTaxing()
+ * @method string|null getDateOfVatApplication()
+ * @method string|null getDescription()
+ * @method string|null getDiscount()
+ * @method string|null getDocumentNumber()
+ * @method string|null getDocumentSerialNumber()
+ * @method string|null getEetResponsibility()
+ * @method string|null getExchangeRate()
+ * @method string|null getExchangeRateAmount()
+ * @method string|null getExported()
+ * @method string|null getId()
+ * @method string|null getIsEet()
+ * @method string|null getIsSentToAccountant()
+ * @method string|null getIsSentToPurchaser()
+ * @method string|null getIssuedInvoiceItems()
+ * @method string|null getItemsTextPrefix()
+ * @method string|null getItemsTextSuffix()
+ * @method string|null getLanguageId()
+ * @method string|null getLinks()
+ * @method string|null getMaturity()
+ * @method string|null getMyCompanyDocumentAdrressId()
+ * @method string|null getNote()
+ * @method string|null getNumericSequenceId()
+ * @method string|null getOrderNumber()
+ * @method string|null getPaymentOptionId()
+ * @method string|null getPaymentStatus()
+ * @method string|null getPurchaserDocumentAddressId()
+ * @method string|null getPurchaserId()
+ * @method string|null getRemindersCount()
+ * @method string|null getReportColorValue()
+ * @method string|null getReportId()
+ * @method string|null getRoundingDifference()
+ * @method string|null getTaxBasicRate()
+ * @method string|null getTaxBasicRateHc()
+ * @method string|null getTaxReducedRate1()
+ * @method string|null getTaxReducedRate1Hc()
+ * @method string|null getTaxReducedRate2()
+ * @method string|null getTaxReducedRate2Hc()
+ * @method string|null getTotalBasicRate()
+ * @method string|null getTotalBasicRateHc()
+ * @method string|null getTotalReducedRate1()
+ * @method string|null getTotalReducedRate1Hc()
+ * @method string|null getTotalReducedRate2()
+ * @method string|null getTotalReducedRate2Hc()
+ * @method string|null getTotalVat()
+ * @method string|null getTotalVatHc()
+ * @method string|null getTotalWithoutVat()
+ * @method string|null getTotalWithoutVatHc()
+ * @method string|null getTotalWithVat()
+ * @method string|null getTotalWithVatHc()
+ * @method string|null getVariableSymbol()
+ * @method string|null getVatOnPayStatus()
+ * @method string|null getVatRateBasic()
+ * @method string|null getVatRateReduced1()
+ * @method string|null getVatRateReduced2()
+ *
+ * @author Lukáš Brzák <lukas.brzak@aquadigital.cz>
+ */
+class IssuedInvoiceApiModel extends iDokladAbstractModel
+{
+    public $AttachmentFileName;
+    public $BaseTaxBasicRate;
+    public $BaseTaxBasicRateHc;
+    public $BaseTaxReducedRate1;
+    public $BaseTaxReducedRate1Hc;
+    public $BaseTaxReducedRate2;
+    public $BaseTaxReducedRate2Hc;
+    public $BaseTaxZeroRate;
+    public $BaseTaxZeroRateHc;
+    public $ConstantSymbolId;
+    public $CurrencyId;
+    public $DateLastChange;
+    public $DateOfAccountingEvent;
+    public $DateOfIssue;
+    public $DateOfLastReminder;
+    public $DateOfMaturity;
+    public $DateOfPayment;
+    public $DateOfTaxing;
+    public $DateOfVatApplication;
+    public $Description;
+    public $Discount;
+    public $DocumentNumber;
+    public $DocumentSerialNumber;
+    public $EetResponsibility;
+    public $ExchangeRate;
+    public $ExchangeRateAmount;
+    public $Exported;
+    public $Id;
+    public $IsEet;
+    public $IsSentToAccountant;
+    public $IsSentToPurchaser;
+    public $IssuedInvoiceItems;
+    public $ItemsTextPrefix;
+    public $ItemsTextSuffix;
+    public $LanguageId;
+    public $Links;
+    public $Maturity;
+    public $MyCompanyDocumentAdrressId;
+    public $Note;
+    public $NumericSequenceId;
+    public $OrderNumber;
+    public $PaymentOptionId;
+    public $PaymentStatus;
+    public $PurchaserDocumentAddressId;
+    public $PurchaserId;
+    public $RemindersCount;
+    public $ReportColorValue;
+    public $ReportId;
+    public $RoundingDifference;
+    public $TaxBasicRate;
+    public $TaxBasicRateHc;
+    public $TaxReducedRate1;
+    public $TaxReducedRate1Hc;
+    public $TaxReducedRate2;
+    public $TaxReducedRate2Hc;
+    public $TotalBasicRate;
+    public $TotalBasicRateHc;
+    public $TotalReducedRate1;
+    public $TotalReducedRate1Hc;
+    public $TotalReducedRate2;
+    public $TotalReducedRate2Hc;
+    public $TotalVat;
+    public $TotalVatHc;
+    public $TotalWithoutVat;
+    public $TotalWithoutVatHc;
+    public $TotalWithVat;
+    public $TotalWithVatHc;
+    public $VariableSymbol;
+    public $VatOnPayStatus;
+    public $VatRateBasic;
+    public $VatRateReduced1;
+    public $VatRateReduced2;
+}
