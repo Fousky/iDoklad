126ccbc24059ee3613c596babcad3aab1d649f63
Lukáš Brzák <lukas.brzak@aquadigital.cz>
2017-08-20 19:14:17 +0200

    GetCashVoucherPdfCompressed functionality

---
+++ src/Model/CashVoucher/CashVoucherPdfCompressedModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Fousky\Component\iDoklad\Model\CashVoucher;
+
+use Fousky\Component\iDoklad\Model\iDokladAbstractModel;
+use Fousky\Component\iDoklad\Model\iDokladModelInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @author Lukáš Brzák <brzak@fousky.cz>
+ */
+class CashVoucherPdfCompressedModel extends iDokladAbstractModel
+{
+    /** @var null|string $zip Raw Base64 ALREADY DECODED data. */
+    public $zip;
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return iDokladModelInterface
+     */
+    public static function createFromResponse(ResponseInterface $response): iDokladModelInterface
+    {
+        $raw = $response->getBody()->getContents();
+
+        $model = new static();
+        $model->zip = base64_decode($raw);
+
+        return $model;
+    }
+
+    /**
+     * @param string $fileName Absolute path to ZIP file to be saved.
+     *
+     * @return bool If saving was successfully
+     */
+    public function save(string $fileName): bool
+    {
+        return file_put_contents($fileName, $this->zip) !== false;
+    }
+}
