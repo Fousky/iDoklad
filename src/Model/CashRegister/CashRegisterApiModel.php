dc8aedeabe31efbe18b528d9af755ef5a324c3c2
Lukáš Brzák <lukas.brzak@aquadigital.cz>
2017-08-20 13:27:59 +0200

    GetCashRegisterExpand functionality

--- src/Model/CashRegister/CashRegisterApiModel.php
+++ src/Model/CashRegister/CashRegisterApiModel.php
@@ -2,9 +2,12 @@
 
 namespace Fousky\Component\iDoklad\Model\CashRegister;
 
+use Fousky\Component\iDoklad\Model\Currencies\CurrencyModel;
 use Fousky\Component\iDoklad\Model\iDokladAbstractModel;
+use Fousky\Component\iDoklad\Model\iDokladModelInterface;
 
 /**
+ * @method null|CurrencyModel getCurrency()
  * @method null|int getCurrencyId()
  * @method null|\DateTime getDateInitialState()
  * @method null|int getId()
@@ -15,11 +18,33 @@ use Fousky\Component\iDoklad\Model\iDokladAbstractModel;
  */
 class CashRegisterApiModel extends iDokladAbstractModel
 {
+    public $Currency;
     public $CurrencyId;
     public $DateInitialState;
     public $Id;
     public $InitialState;
     public $Name;
+
+    /**
+     * @param \stdClass $data
+     *
+     * @return iDokladModelInterface
+     */
+    public static function createFromStd(\stdClass $data): iDokladModelInterface
+    {
+        /** @var CashRegisterApiModel $model */
+        $model = parent::createFromStd($data);
+
+        if ($model->Currency instanceof \stdClass) {
+            $model->Currency = CurrencyModel::createFromStd($model->Currency);
+
+            if ($model->getCurrency() instanceof CurrencyModel) {
+                $model->CurrencyId = $model->getCurrency()->getId();
+            }
+        }
+
+        return $model;
+    }
 
     /**
      * @return array
