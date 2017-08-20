ce4115822f2038a7ddde63a330b465424ad9ed86
Lukáš Brzák <lukas.brzak@aquadigital.cz>
2017-08-20 11:50:44 +0200

    GetAgendaSummary functionality

---
+++ src/Functions/Agendas/GetAgendaSummary.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Fousky\Component\iDoklad\Functions\Agendas;
+
+use Fousky\Component\iDoklad\Functions\iDokladAbstractFunction;
+use Fousky\Component\iDoklad\Model\Agendas\AgendaSummaryModel;
+
+/**
+ * @author Lukáš Brzák <lukas.brzak@aquadigital.cz>
+ */
+class GetAgendaSummary extends iDokladAbstractFunction
+{
+    /**
+     * Get iDokladModelInterface class.
+     *
+     * @see iDokladModelInterface
+     *
+     * @return string
+     */
+    public function getModelClass(): string
+    {
+        return AgendaSummaryModel::class;
+    }
+
+    /**
+     * GET|POST|PUT|DELETE e.g.
+     *
+     * @see iDoklad::request()
+     *
+     * @return string
+     */
+    public function getHttpMethod(): string
+    {
+        return 'GET';
+    }
+
+    /**
+     * Return base URI, e.g. /invoices; /invoice/1/edit and so on.
+     *
+     * @see iDoklad::call()
+     *
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return 'Agendas/GetAgendaSummary';
+    }
+
+    /**
+     * Vrátí seznam parametrů, které se předají GuzzleHttp\Client
+     *
+     * @see \GuzzleHttp\Client::request()
+     * @see iDoklad::call()
+     *
+     * @return array
+     */
+    public function getGuzzleOptions(): array
+    {
+        return [];
+    }
+}
---
+++ src/Model/Agendas/AgendaSummaryModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Fousky\Component\iDoklad\Model\Agendas;
+
+use Fousky\Component\iDoklad\Model\iDokladAbstractModel;
+
+/**
+ * @method int|null getContacts()
+ * @method int|null getIssuedInvoices()
+ * @method int|null getPriceListItems()
+ *
+ * @author Lukáš Brzák <lukas.brzak@aquadigital.cz>
+ */
+class AgendaSummaryModel extends iDokladAbstractModel
+{
+    public $Contacts;
+    public $IssuedInvoices;
+    public $PriceListItems;
+}
