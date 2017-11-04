e9f239a2492c2cdb756cc6c0fd66e6c0f29793e0
Lukáš Brzák <lukas.brzak@aquadigital.cz>
2017-11-05 00:47:03 +0100

    RecountSalesReceipt functionality.

---
+++ Functions/SalesReceipts/RecountSalesReceipt.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Fousky\Component\iDoklad\Functions\SalesReceipts;
+
+use Fousky\Component\iDoklad\Functions\iDokladAbstractFunction;
+use Fousky\Component\iDoklad\Model\iDokladAbstractModel;
+use Fousky\Component\iDoklad\Model\SalesReceipts\SalesReceiptApiModel;
+use Fousky\Component\iDoklad\Model\SalesReceipts\SalesReceiptApiModelPost;
+
+/**
+ * @author Lukáš Brzák <brzak@fousky.cz>
+ */
+class RecountSalesReceipt extends iDokladAbstractFunction
+{
+    /** @var SalesReceiptApiModelPost $data */
+    protected $data;
+
+    /**
+     * @param SalesReceiptApiModelPost $data
+     *
+     * @throws \Fousky\Component\iDoklad\Exception\InvalidModelException
+     */
+    public function __construct(SalesReceiptApiModelPost $data)
+    {
+        $this->data = $data;
+
+        $this->validate($data);
+    }
+
+    /**
+     * Get iDokladModelInterface class.
+     *
+     * @see iDokladModelInterface
+     *
+     * @return string
+     */
+    public function getModelClass(): string
+    {
+        return SalesReceiptApiModel::class;
+    }
+
+    /**
+     * GET|POST|PUT|DELETE e.g.
+     *
+     * @see iDoklad::request()
+     *
+     * @return string
+     */
+    public function getHttpMethod(): string
+    {
+        return 'POST';
+    }
+
+    /**
+     * Return base URI, e.g. /invoices; /invoice/1/edit and so on.
+     *
+     * @see iDoklad::call()
+     *
+     * @return string
+     */
+    public function getUri(): string
+    {
+        return 'SalesReceipts/Recount';
+    }
+
+    /**
+     * Vrátí seznam parametrů, které se předají GuzzleHttp\Client.
+     *
+     * @see \GuzzleHttp\Client::request()
+     * @see iDoklad::call()
+     *
+     * @return array
+     * @throws \ReflectionException
+     * @throws \InvalidArgumentException
+     */
+    public function getGuzzleOptions(): array
+    {
+        return [
+            'json' => $this->data->toArray([
+                iDokladAbstractModel::TOARRAY_REMOVE_NULLS => true,
+            ]),
+        ];
+    }
+}
